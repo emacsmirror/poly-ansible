2cb970a0e27b41ae85bc51d24ef36fa2c7b34bbc
Peter Oliver <git@mavit.org.uk>
2018-12-22 15:17:35 +0000

    Enable `ansible` minor-mode for additional highlighting and features.

--- poly-ansible.el
+++ poly-ansible.el
@@ -17,7 +17,7 @@
 
 ;; Author: Peter Oliver <poly-ansible@mavit.org.uk>
 ;; Version: 0.3
-;; Package-Requires: ((ansible-doc "0.4") (jinja2-mode "0.2") (polymode "0.1.5") (yaml-mode "0.0.13"))
+;; Package-Requires: ((ansible) (ansible-doc "0.4") (jinja2-mode "0.2") (polymode "0.1.5") (yaml-mode "0.0.13"))
 ;; Keywords: languages
 ;; URL: https://gitlab.com/mavit/poly-ansible/
 
@@ -46,6 +46,7 @@
   :hostmode 'pm-host/yaml
   :innermodes '(pm-inner/jinja2)
 
+  (ansible 1)
   (ansible-doc-mode 1))
 
 
