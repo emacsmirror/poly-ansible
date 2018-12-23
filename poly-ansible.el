425ba6fc9efefd1cf08fa5c97ed1cd7d4a08d1a3
Peter Oliver <git@mavit.org.uk>
2018-12-23 11:37:54 +0000

    Specify tested ansible minor mode version.

--- poly-ansible.el
+++ poly-ansible.el
@@ -17,7 +17,7 @@
 
 ;; Author: Peter Oliver <poly-ansible@mavit.org.uk>
 ;; Version: 0.3
-;; Package-Requires: ((ansible) (ansible-doc "0.4") (jinja2-mode "0.2") (polymode "0.1.5") (yaml-mode "0.0.13"))
+;; Package-Requires: ((ansible "0.2") (ansible-doc "0.4") (jinja2-mode "0.2") (polymode "0.1.5") (yaml-mode "0.0.13"))
 ;; Keywords: languages
 ;; URL: https://gitlab.com/mavit/poly-ansible/
 
