036c51fab14038f7613f77daee300e74efe7da6a
Peter Oliver <git@mavit.org.uk>
2024-04-17 15:01:48 +0100

    Bump version number.

--- poly-ansible.el
+++ poly-ansible.el
@@ -18,7 +18,7 @@
 ;; along with poly-ansible.  If not, see <https://www.gnu.org/licenses/>.
 ;;
 ;; Author: Peter Oliver <poly-ansible@mavit.org.uk>
-;; Version: 0.3
+;; Version: 0.3.1
 ;; Package-Requires: ((ansible "0.2") (ansible-doc "0.4") (jinja2-mode "0.2") (polymode "0.1.5") (yaml-mode "0.0.13"))
 ;; Keywords: languages
 ;; URL: https://gitlab.com/mavit/poly-ansible/
